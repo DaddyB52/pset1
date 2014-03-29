170b460fbec7191243b56ce71868bc85eece7d56
MB-Jharvard <jharvard@applianceLCSTL.com>
2014-03-28 23:44:32 -0400

    User Input For Half-Pyramid Block Height Works

--- mario.c
+++ mario.c
@@ -3,7 +3,15 @@
 
 int main(void)
 {
-    printf("Okey Doke, Mario, You Little Twerp, Time To Jump The F@#$%!!!!! Blocks\n");
+    printf("Okey Doke, Time to make Mario, That Little Twerp, Jump The F@#$!!!!! Blocks. Please give me an int for the height of the blocks Mario TLT will jump:\n");//Changed 1st printf command to prompt user for a integer.  Deleted 2nd printf command.
+    int n = GetInt();
     
-    printf("Listen, You Little Dork, You're Not Moving. Let's Get Your Act Together Here, Dude\n");
+    if (n >=0 && n <= 23)
+    {
+        printf("Thank you, you picked a useable height for the half-pyramid of blocks!\n");
+     }
+     else
+     {
+        printf("Sorry, that number won't work, please try again!\n");
+     }
 }
